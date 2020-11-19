181db03d6b5bc8da460922b800e682d96ddd762f
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-11-19 21:48:25 +0100

    run.sh: compile and run

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo "Compiling..."
+export CP='.:../jars/*:../jars/commons-math3-3.5/*'
+
+cd src
+
+echo Classpath: $CP
+find . -name "*.java" -print | xargs javac -cp $CP
+
+echo "DONE"
+
+echo "Running..."
+
+java -classpath $CP org.fog.test.perfeval.VRGameFog
